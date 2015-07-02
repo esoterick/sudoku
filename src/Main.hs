a994c24f8d9e8baae38392f47ea448a7fc92082c
Robert J. Lambert III <rob@rlambert3.com>
2015-07-01 22:22:08 -0400

    Fixed the wichSubMatrix function with the correct calculation and fixed the puzzle function to use it

--- src/Main.hs
+++ src/Main.hs
@@ -2,18 +2,20 @@
 -- Copyright (c) 2006 Don Stewart - http://www.cse.unsw.edu.au/~dons/
 -- GPL version 2 or later (see http://www.gnu.org/copyleft/gpl.html)
 --
-import System.Environment
 import Data.Matrix
 
+main :: IO()
 main = do putStrLn "rage"
-          putStrLn $ prettyMatrix $ zero 9 9
+          -- putStrLn $ prettyMatrix $ zero 9 9
+          putStrLn $ prettyMatrix $ fromList 9 9 [1..]
           putStrLn $ puzzle 1
 
-data Block = Int | Nothing
-
 puzzle :: Int -> String
-puzzle _ = prettyMatrix $ submatrix 1 3 1 3 m
+puzzle _ = prettyMatrix $ m2
+           -- prettyMatrix $ submatrix 1 3 1 3 m
+
     where m = fromList 9 9 [1..]
+          m2 = getSubMatrix (whichSubMatrix (5,5)) m
 
 whichSubMatrix :: (Int, Int) -> (Int, Int)
 whichSubMatrix (row, col) = (r, c)
@@ -22,8 +24,8 @@ whichSubMatrix (row, col) = (r, c)
 
 getSubMatrix :: (Int, Int) -> Matrix a -> Matrix a
 getSubMatrix (row, col) primaryMatrix = subMatrix
-    where rowStart = (row * 3) - 3
+    where rowStart = (row * 3) - 2
           rowEnd = row * 3
-          colStart = (col * 3) - 3
+          colStart = (col * 3) - 2
           colEnd = col * 3
           subMatrix = submatrix rowStart rowEnd colStart colEnd primaryMatrix
